a25cee3f47e5a6dafe56c2fd2440ce3fe21fbc46
Douglas Tardioli <douglastardioli48@gmail.com>
2023-06-21 10:01:44 -0300

    Add files via upload

---
+++ data/produtos.ts
@@ -0,0 +1,118 @@
+import Produto from "@/model/Produto"
+
+const Produtos
+: Produto[] = [
+  {
+    id: 1,
+    nome: 'Smart TV LED 43" 4K UHD Samsung UN43AU7700GXZD - Alexa built-in, Bivolt',
+    preco: 1159.95,
+    Imagem: "https://m.media-amazon.com/images/I/81UGhjKIQdL._AC_SL1500_.jpg",
+    Link: "https://www.amazon.com.br/UN43AU7700GXZD-SAMGUNG-TV-CRYSTAL-UHD/dp/B09QV7PZM6?ref_=Oct_d_obs_d_16243822011_2&pd_rd_w=5XDC4&content-id=amzn1.sym.21fe3f75-8033-45e9-8f13-11fbfa17d135&pf_rd_p=21fe3f75-8033-45e9-8f13-11fbfa17d135&pf_rd_r=D0T4PGEX59YN1SHGY571&pd_rd_wg=HjE1s&pd_rd_r=f9327ce3-17cf-48da-8d91-4562b7f79a2e&pd_rd_i=B09QV7PZM6"
+  },
+  {
+    id: 2,
+    nome: 'Smart TV LED 32" HD AOC ROKU TV FHD 32S5195/78G, Wi-Fi, 3 HDMI, 1 USB, Wifi, Conversor Digital',
+    preco: 1179.01,
+    Imagem: "https://m.media-amazon.com/images/I/51oNweG3tiL._AC_SL1000_.jpg",
+    Link: "https://www.amazon.com.br/SMART-AOC-ROKU-32S5195-78G/dp/B08487GGBX/?_encoding=UTF8&pd_rd_w=lWk97&content-id=amzn1.sym.3c1cbf54-971c-48a3-9880-ef2d6a30efac&pf_rd_p=3c1cbf54-971c-48a3-9880-ef2d6a30efac&pf_rd_r=1DR3Y2PBNXPYHNTD1ZR2&pd_rd_wg=FOHKX&pd_rd_r=41e05453-e26c-4705-b354-1662fdc1c529&ref_=pd_gw_deals_vi_t1"
+
+  },
+  {
+    id: 3,
+    nome: "Sabonete Demaquilante Pós Maquiagem Make Out Dermachem, Dermachem",
+    preco: 8.99,
+    Imagem: "https://m.media-amazon.com/images/I/51-kwxfdHtL._AC_SL1000_.jpg",
+    Link: "https://www.amazon.com.br/Make-Out-Maquiagem-Demaquilante-Dermachem/dp/B07VWC6KGD/ref=sr_1_3?__mk_pt_BR=%C3%85M%C3%85%C5%BD%C3%95%C3%91&crid=10IHGID0RNSK8&keywords=make&qid=1687256640&sprefix=make%2Cspecialty-aps%2C241&sr=8-3"
+
+  },
+  {
+    id: 4,
+    nome: "Bt Contorno Brown Sugar, Bruna Tavares",
+    preco: 42.90,
+    Imagem: "https://m.media-amazon.com/images/I/71QJp07PqVL._AC_SL1500_.jpg",
+    Link: "https://www.amazon.com.br/Contorno-Brown-Sugar-Bruna-Tavares/dp/B08RL99MSN/ref=sr_1_13?__mk_pt_BR=%C3%85M%C3%85%C5%BD%C3%95%C3%91&crid=10IHGID0RNSK8&keywords=make&qid=1687256715&sprefix=make%2Cspecialty-aps%2C241&sr=8-13"
+
+  },
+  {
+    id: 5,
+    nome: "Océane PALETA DE SOMBRAS, LARISA MANOELA, FABULOUS EYESHADOW PALETTE, 6 cores",
+    preco: 42.90,
+    Imagem: "https://m.media-amazon.com/images/I/51IkmFXf0JL._AC_SL1200_.jpg",
+    Link: "https://vscode-file://vscode-app/c:/Users/User/AppData/Local/Programs/Microsoft%20VS%20Code/resources/app/out/vs/code/electron-sandbox/workbench/workbench.htmlwww.amazon.com.br/UN43AU7700GXZD-SAMGUNG-TV-CRYSTAL-UHD/dp/B09QV7PZM6?ref_=Oct_d_obs_d_16243822011_2&pd_rd_w=5XDC4&content-id=amzn1.sym.21fe3f75-8033-45e9-8f13-11fbfa17d135&pf_rd_p=21fe3f75-8033-45e9-8f13-11fbfa17d135&pf_rd_r=D0T4PGEX59YN1SHGY571&pd_rd_wg=HjE1s&pd_rd_r=f9327ce3-17cf-48da-8d91-4562b7f79a2e&pd_rd_i=B09QV7PZM6"
+
+  },
+  {
+    id: 6,
+    nome: "Primer Facial Vegano Anita, Anita",
+    preco: 10.90,
+    Imagem: "https://m.media-amazon.com/images/I/41-9uwWWrzL._AC_SL1000_.jpg",
+    Link: "https://www.amazon.com.br/Primer-Facial-Vegano-Anita/dp/B09VCR81FT/ref=sr_1_19?__mk_pt_BR=%C3%85M%C3%85%C5%BD%C3%95%C3%91&crid=10IHGID0RNSK8&keywords=make&qid=1687256715&sprefix=make%2Cspecialty-aps%2C241&sr=8-19"
+
+  },
+  {
+    id: 7,
+    nome: "To Go - Paleta de Sombras-Basic/Basic, Océane",
+    preco: 41.94,
+    Imagem: "https://m.media-amazon.com/images/I/81UXFbXxv0L._AC_SL1500_.jpg",
+    Link: "https://www.amazon.com.br/Go-Paleta-Sombras-Basic-Basic-Oc%C3%A9ane/dp/B08XZ91GH9/ref=sr_1_19?__mk_pt_BR=%C3%85M%C3%85%C5%BD%C3%95%C3%91&crid=LQ8O300RD3DD&keywords=make&qid=1687257160&sprefix=make%2Caps%2C200&sr=8-19"
+
+  },
+  {
+    id: 8,
+    nome: "Mica Rocha Sombra Mono Gêmeos, Tb Make",
+    preco: 35.90,
+    Imagem: "https://m.media-amazon.com/images/I/516rDOmqUzL._AC_SL1500_.jpg",
+    Link: "https://www.amazon.com.br/Mica-Rocha-Sombra-Mono-G%C3%AAmeos/dp/B092X445R2/ref=sr_1_28?__mk_pt_BR=%C3%85M%C3%85%C5%BD%C3%95%C3%91&crid=LQ8O300RD3DD&keywords=make&qid=1687257235&sprefix=make%2Caps%2C200&sr=8-28"
+
+  },
+  {
+    id: 9,
+    nome: "Fdk Blush Compacto Pink Lemonade, FREDERIKA",
+    preco: 22.90,
+    Imagem: "https://m.media-amazon.com/images/I/61Y4vOwVy5S._AC_SL1000_.jpg",
+    Link: "https://www.amazon.com.br/Blush-Compacto-Pink-Lemonade-FREDERIKA/dp/B095ZG4CD2/ref=sr_1_35?__mk_pt_BR=%C3%85M%C3%85%C5%BD%C3%95%C3%91&crid=LQ8O300RD3DD&keywords=make&qid=1687257235&sprefix=make%2Caps%2C200&sr=8-35"
+
+  },
+  {
+    id: 10,
+    nome: "Pincel Profissional kabuki reto - Linha R - R-07, Macrilan",
+    preco: 28.90,
+    Imagem: "https://m.media-amazon.com/images/I/61Lc+YR911L._AC_SL1500_.jpg",
+    Link: "https://www.amazon.com.br/Pincel-Profissional-kabuki-reto-Macrilan/dp/B08FYCFDRK/ref=sr_1_37?__mk_pt_BR=%C3%85M%C3%85%C5%BD%C3%95%C3%91&crid=LQ8O300RD3DD&keywords=make&qid=1687257235&sprefix=make%2Caps%2C200&sr=8-37"
+
+  },
+  {
+    id: 11,
+    nome: "Esponja Vinho de Maquiagem Mariana Saad - Sister Blend By Océane",
+    preco: 24.49,
+    Imagem: "https://m.media-amazon.com/images/I/51acp+s3LfS._AC_SL1000_.jpg",
+    Link: "https://www.amazon.com.br/Esponja-Vinho-Maquiagem-Mariana-Saad/dp/B08HRC21F7/ref=sr_1_42?__mk_pt_BR=%C3%85M%C3%85%C5%BD%C3%95%C3%91&crid=LQ8O300RD3DD&keywords=make&qid=1687257235&sprefix=make%2Caps%2C200&sr=8-42"
+
+  },
+  {
+    id: 12,
+    nome: "Esponja Vinho de Maquiagem Mariana Saad - Sister Blend By Océane",
+    preco: 24.49,
+    Imagem: "https://m.media-amazon.com/images/I/51acp+s3LfS._AC_SL1000_.jpg",
+    Link: "https://www.amazon.com.br/Esponja-Vinho-Maquiagem-Mariana-Saad/dp/B08HRC21F7/ref=sr_1_42?__mk_pt_BR=%C3%85M%C3%85%C5%BD%C3%95%C3%91&crid=LQ8O300RD3DD&keywords=make&qid=1687257235&sprefix=make%2Caps%2C200&sr=8-42"
+
+  },
+  {
+    id: 13,
+    nome: "Frederika Base Matte Vegana 01, Frederika",
+    preco: 35.59,
+    Imagem: "https://m.media-amazon.com/images/I/51N6dRaLqJS._AC_SL1000_.jpg",
+    Link: "https://www.amazon.com.br/Frederika-Base-Matte-Vegana-01/dp/B095ZF9VXV/ref=sr_1_48?__mk_pt_BR=%C3%85M%C3%85%C5%BD%C3%95%C3%91&crid=LQ8O300RD3DD&keywords=make&qid=1687257235&sprefix=make%2Caps%2C200&sr=8-48"
+
+  },
+  {
+    id: 14,
+    nome: "Batom Glossy Rosa Rose Make B. 3,6g O Boticário",
+    preco:72.99,
+    Imagem: "https://m.media-amazon.com/images/I/31lrRHFs-oL._AC_.jpg",
+    Link: "https://www.amazon.com.br/Batom-Glossy-Rosa-Rose-Botic%C3%A1rio/dp/B0C5Z58MLR/ref=sr_1_52?__mk_pt_BR=%C3%85M%C3%85%C5%BD%C3%95%C3%91&crid=LQ8O300RD3DD&keywords=make&qid=1687257584&sprefix=make%2Caps%2C200&sr=8-52"
+
+  },
+];
+
+export default Produtos
